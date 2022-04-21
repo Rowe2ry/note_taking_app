caf2f81785e5ffe93d60bc8abf0928653518753f
Chris Rowe <rowe2ry@yahoo.com>
2022-04-21 16:14:41 -0400

    Post method server side handler working well.

--- routes/notes.js
+++ routes/notes.js
@@ -2,10 +2,10 @@
  * Module Imports && Configuration
  * ========================================================================= */
 
-const fs = require('fs');
-// const util = require('util');
-const notes = require('express').Router();
-const jsonDb = require('../db/db.json');
+const fs = require('fs'); // file system
+const notes = require('express').Router();  // use 'notes' as a router
+const jsonDb = require('../db/db.json');  // this is the file we want to work with
+const { nanoid } = require('nanoid'); // random id generator
 
 notes.get('/', (req,res) => {
     console.info(`${req.method} request for db.json received at /api/notes`);
@@ -15,24 +15,34 @@ notes.get('/', (req,res) => {
 notes.post('/', (req,res) => {
     console.info(`${req.method} request for adding a new note received at /api/notes`);
 
-    const { title , text } = req.body;
+    const { title , text } = req.body; // object destructuring from the request
 
-    if (title && text) {
-        const newNote = {
+    if (title && text) { // if the formatting is valid...
+        const newNote = { // pull the info from the input
             title,
-            text
-        };
+            text,
+            id: nanoid() // random id generator
+        }; 
+        
+        console.info(`The input title is: ${title} \n and \n the input text is: ${text}`);
+        
+        fs.readFile('./db/db.json', 'utf8', (err, data) => { // open the current JSON faux database
+            if (err) {
+                console.error(err); // if we get an error display it
+              } else {
+                const parsedData = JSON.parse(data); // turn our current JSON into an object
+                parsedData.push(newNote); // ad the newest info we just got from the user to the array
+                fs.writeFile('./db/db.json', JSON.stringify(parsedData, null, 4), (err) =>
+                err ? console.error(err) : console.info(`\nData added!`)
+                );
+              };
+        });
+
+        res.json('Roger that, we have made contact.');
+    } else {
+        res.error('Oh no something went wrong.');
     };
 
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) {
-            console.error(err);
-          } else {
-            const parsedData = JSON.parse(data);
-            parsedData.push(content);
-            writeToFile(file, parsedData);
-          };
-    });
 });
 
 module.exports = notes;
