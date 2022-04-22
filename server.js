efd736e620acf9559b52682972dcf679fe9e9751
Chris Rowe <rowe2ry@yahoo.com>
2022-04-21 21:45:44 -0400

    Trying nodemon to solve front end issues.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
  * Module Imports && Configuration
  * ========================================================================= */
 
+const nodemon = require('nodemon');
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -32,9 +33,9 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-/*app.get('*', (req, res) => 
+app.get('*', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
-);*/
+);
 
 app.listen(PORT, () => {
     console.info(`server live and listening at http://localhost:${PORT}`);
